166c97963c3d85608b97acda0f5ce89314f5dbf6
Patrick <teamgeist2007@gmail.com>
2026-09-13 20:47:00 +0200

    changed all

---
+++ script.js
@@ -0,0 +1,32 @@
+const dishes = [
+  {
+    name: "Pizza Margherita",
+    description: "Mit Basilikum und Tomaten",
+    price: 9.90,
+    image: "./assets/img/Pizza Margherita.png"
+  },
+  {
+    name: "Pizza Salami",
+    description: "Mit Salami, Chiliflocken und Parmesan",
+    price: 11.90,
+    image: "./assets/img/Pizza Salami.png"
+  },
+  {
+    name: "Pizza Funghi",
+    description: "Mit frischen Champignons",
+    price: 11.90,
+    image: "./assets/img/Pizza Funghi.png"
+  },
+  {
+    name: "Pizza Diavola",
+    description: "Mit Chili und Jalapeños",
+    price: 12.90,
+    image: "./assets/img/Pizza Diavola.png"
+  },
+  {
+    name: "Pizza Quattro Formaggi",
+    description: "Mit Gorgonzola und geriebenem Käse",
+    price: 13.90,
+    image: "./assets/img/Pizza Quattro Formaggi.png"
+  }
+];
