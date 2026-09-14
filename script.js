9ab815ea5f0a7d057ff5730562dcb2e235bbce72
Patrick <teamgeist2007@gmail.com>
2026-09-14 16:01:22 +0200

    changed all

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const dishes = [
   {
     name: "Pizza Funghi",
     description: "Mit frischen Champignons",
-    price: 11.90,
+    price: 9.90,
     image: "./assets/img/Pizza Funghi.png"
   },
   {
@@ -26,7 +26,34 @@ const dishes = [
   {
     name: "Pizza Quattro Formaggi",
     description: "Mit Gorgonzola und geriebenem Käse",
-    price: 13.90,
+    price: 10.90,
     image: "./assets/img/Pizza Quattro Formaggi.png"
   }
 ];
+
+function renderDishes() {
+  const dishList = document.getElementById("dishes-list");
+  dishList.innerHTML = "";
+
+  for (let i = 0; i < dishes.length; i++) {
+    dishList.innerHTML += getDishTemplate(dishes[i]);
+  }
+}
+
+function getDishTemplate(dish) {
+  return `
+    <div class="dishCard">
+      <img src="${dish.image}" alt="${dish.name}">
+
+      <div class="dishInfo">
+        <h3>${dish.name}</h3>
+        <p>${dish.description}</p>
+        <strong>${dish.price.toFixed(2).replace(".", ",")} €</strong>
+      </div>
+
+      <button class="addButton">Add to basket</button>
+    </div>
+  `;
+}
+
+renderDishes();
