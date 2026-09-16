0c2dc61fdf22637eccdd493735c66ec79bf7f59f
Patrick <teamgeist2007@gmail.com>
2026-09-16 11:55:26 +0200

    changed js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const dishes = [
   {
     name: "Pizza Margherita",
     description: "Mit Tomatensoße, Mozzarella und Basilikum",
-    price: 9.90,
+    price: 8.90,
     image: "./assets/img/Pizza Margherita.png"
   },
   {
     name: "Pizza Salami",
-    description: "Mit Tomatensoße, Salami und geriebenem Käse",
+    description: "Mit Tomatensoße, Salami und geriebenen Käse",
     price: 11.90,
     image: "./assets/img/Pizza Salami.png"
   },
   {
     name: "Pizza Funghi",
-    description: "Mit Tomatensoße, frischen Champignons und geriebenem Käse",
-    price: 10.90,
+    description: "Mit Tomatensoße, frischen Champignons und geriebenen Käse",
+    price: 9.90,
     image: "./assets/img/Pizza Funghi.png"
   },
   {
@@ -26,7 +26,7 @@ const dishes = [
   {
     name: "Pizza Quattro Formaggi",
     description: "Mit Tomatensoße, Mozzarella, Gorgonzola, Parmesan und Fontina",
-    price: 11.90,
+    price: 10.90,
     image: "./assets/img/Pizza Quattro Formaggi.png"
   }
 ];
